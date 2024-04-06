d0e44c8a4d0679880a0e2042cd01851a2da16858
Divyam2585 <divyamkumari2585@gmail.com>
2024-04-06 21:09:54 +0530

    Create sign in API route and complete sign page functionality

--- api/index.js
+++ api/index.js
@@ -17,22 +17,23 @@ mongoose
     });
 
 const app=express();
+
 app.use(express.json());
+
 app.listen(3000,()=>{
     console.log('Server is running on port 3000!');
     }
 );
 
-app.use('/api/user', userRouter);
-app.use('/api/auth', authRouter);
+app.use("/api/user",userRouter);
+app.use('/api/auth',authRouter);
 
-app.use((err, req, res, next)=>{
-    const statusCode=err.statusCode || 500;
-    const message=err.message || 'Internal Server Error';
-    return res.status(statusCode).json({
-       sucess: false,
-       statusCode,
-       message,
-    });
-}
-);
+app.use((err, req, res, next) => {
+	const statusCode = err.statusCode || 500;
+	const message = err.message || 'Internal Server Error';
+	return res.status(statusCode).json({
+		success: false,
+		statusCode,
+		message,
+	});
+});
